b5cddee583d6728dd87af27d334f58779680d19f
PiStefania <stefania.patsou@hotmail.com>
2021-12-07 18:02:41 +0000

    Execute all possible vfl cases

--- CI-script-vfl.sh
+++ CI-script-vfl.sh
@@ -3,11 +3,13 @@
 set -ex
 
 # code checking
-#pyflakes .
+#python3 -m pyflakes .
 
 # activate the fedml environment
 source "$HOME/miniconda/etc/profile.d/conda.sh"
 conda activate fedml
+
+conda install pandas
 
 wandb login ee0b5f53d949c84cee7decbe7a629e63fb2f8408
 wandb off
@@ -30,9 +32,10 @@ round() {
 }
 
 ## Prepare NUS WIDE dataset
-python3 ./fedml_api/data_preprocessing/NUS_WIDE/nus_wide_dataset.py
+cd ./fedml_api/data_preprocessing/NUS_WIDE
+python3 nus_wide_dataset.py
 
-cd ./fedml_experiments/standalone/classical_vertical_fl
+cd ../../../fedml_experiments/standalone/classical_vertical_fl
 
 if [ ! -d "./logs" ] 
 then
@@ -40,10 +43,10 @@ then
 fi
 
 ## Lending club load - two party
-python3 run_vfl_fc_two_party_lending_club.py > ./logs/vfl_two_party_lending_club_logs.txt
+python3 run_vfl_fc_two_party_lending_club.py > ./logs/vfl_two_party_lending_club_logs.txt 2>&1
 ## Lending club loan - three party
-python3 run_vfl_fc_three_party_lending_club.py > ./logs/vfl_three_party_lending_club_logs.txt
+python3 run_vfl_fc_three_party_lending_club.py > ./logs/vfl_three_party_lending_club_logs.txt 2>&1
 ## NUS WIDE - two party
-python3 run_vfl_fc_two_party_nus_wide.py > ./logs/vfl_two_party_nus_wide_logs.txt
+python3 run_vfl_fc_two_party_nus_wide.py > ./logs/vfl_two_party_nus_wide_logs.txt 2>&1
 ## NUS WIDE - three party
-python3 run_vfl_fc_three_party_nus_wide.py > ./logs/vfl_three_party_nus_wide_logs.txt
+python3 run_vfl_fc_three_party_nus_wide.py > ./logs/vfl_three_party_nus_wide_logs.txt 2>&1
